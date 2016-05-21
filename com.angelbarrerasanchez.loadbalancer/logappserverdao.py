d9671e8fabef63da6ab0bfa97cfb57ac8293e72e
Ángel Barrera Sánchez <angelbarrerasanchez@gmail.com>
2016-05-21 10:26:11 +0200

    added remove log function, capped collection simulation

--- com.angelbarrerasanchez.loadbalancer/logappserverdao.py
+++ com.angelbarrerasanchez.loadbalancer/logappserverdao.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 import constants
-import datetime
+from datetime import datetime, timedelta
 
 mongo = MongoClient(constants.MONGO_URI)
 mongo_db = mongo[constants.MONGO_DATABASE]
@@ -16,12 +16,14 @@ def write_apps_log(app_name, path, end_point, total_time, http_response_code):
         'app_name': app_name,
         'path': path,
         'end_point': end_point,
-        'date': datetime.datetime.utcnow(),
+        'date': datetime.utcnow(),
         'total_time_ms': total_time,
         'response_code': http_response_code
     }
     mongo_collection.insert_one(log)
+    remove_apps_log(1)
 
 
-def remove_apps_log():
-    return None
+def remove_apps_log(max_days):
+    date = datetime.utcnow() + timedelta(days=-max_days)
+    mongo_collection.remove({'date': {"$lt": date}}, multi=True)
